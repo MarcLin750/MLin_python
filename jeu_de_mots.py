74e7f12afa4042c65251f73a4e27747850d95b76
Marc <marclin750@gmail.com>
2023-05-18 19:51:59 +0200

    Ajout des instructions pour afficher la longueur du mot et affichier le mot caché si non découvert

--- jeu_de_mots.py
+++ jeu_de_mots.py
@@ -3,12 +3,16 @@ from random import randint
 DICO = ["ENTRE", "OCTET", "FRUIT", "TAPIS", "PIECE", "PORTE", "VITRE", "NOTES", "BATON"]
 Rang = randint(0, 8)
 MOT = DICO[Rang]
+longueur_mot = len(MOT)
 ANS = False
+
+print("La longueur du mot est:" , longueur_mot)
 
 i = 0
 
 while i < 3 and ANS == False:
-    REP = input("Un mot de 5 lettre:")
+    print("Saisir un mot de", longueur_mot ,"lettre:")
+    REP = input()
 
     if REP == MOT:
         ANS = True
@@ -21,4 +25,10 @@ while i < 3 and ANS == False:
 
     i += 1
 
-print(ANS, MOT)
+print(MOT)
+
+if REP == MOT:
+    print("Bravo tu à trouver le bon mot !")
+
+else:
+    print("Le mot était: ", MOT)
