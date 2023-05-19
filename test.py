631493abd70c59d94668d09ef2ca7105c8c6f581
Marc <marclin750@gmail.com>
2023-05-20 00:09:19 +0200

    Ajout d'un fichier test

---
+++ test.py
@@ -0,0 +1,17 @@
+a = 12
+b = 235
+c = 1341
+
+rep1 = c / a
+
+print("rep1 = c / a")
+print(c , " / " , a , " = " , rep1)
+
+rep2 = b - rep1
+
+print("rep2 = b - rep1")
+print(b , "-" , rep1 , "=" , rep2)
+
+rep3 = rep1 * rep2
+print("rep3 = rep1 * rep2")
+print(rep1 , "*" , rep2 , "=" , rep3)
